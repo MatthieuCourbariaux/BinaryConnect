7e1b8236c773ae927915ef67a16d4f89d6835812
Matthieu Courbariaux <matthieu.courbariaux@gmail.com>
2015-08-21 16:27:17 -0400

    Lasagne seems to work very well (even BN :)

---
+++ batch_norm.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+
+"""
+Preliminary implementation of batch normalization for Lasagne.
+Does not include a way to properly compute the normalization factors over the
+full training set for testing, but can be used as a drop-in for training and
+validation.
+
+Author: Jan Schlüter
+"""
+
+import numpy as np
+import lasagne
+import theano
+import theano.tensor as T
+
+class BatchNormLayer(lasagne.layers.Layer):
+
+    def __init__(self, incoming, axes=None, epsilon=0.01, alpha=0.5,
+            nonlinearity=None, **kwargs):
+        """
+        Instantiates a layer performing batch normalization of its inputs,
+        following Ioffe et al. (http://arxiv.org/abs/1502.03167).
+        
+        @param incoming: `Layer` instance or expected input shape
+        @param axes: int or tuple of int denoting the axes to normalize over;
+            defaults to all axes except for the second if omitted (this will
+            do the correct thing for dense layers and convolutional layers)
+        @param epsilon: small constant added to the standard deviation before
+            dividing by it, to avoid numeric problems
+        @param alpha: coefficient for the exponential moving average of
+            batch-wise means and standard deviations computed during training;
+            the larger, the more it will depend on the last batches seen
+        @param nonlinearity: nonlinearity to apply to the output (optional)
+        """
+        super(BatchNormLayer, self).__init__(incoming, **kwargs)
+        if axes is None:
+            # default: normalize over all but the second axis
+            axes = (0,) + tuple(range(2, len(self.input_shape)))
+        elif isinstance(axes, int):
+            axes = (axes,)
+        self.axes = axes
+        self.epsilon = epsilon
+        self.alpha = alpha
+        if nonlinearity is None:
+            nonlinearity = lasagne.nonlinearities.identity
+        self.nonlinearity = nonlinearity
+        shape = list(self.input_shape)
+        broadcast = [False] * len(shape)
+        for axis in self.axes:
+            shape[axis] = 1
+            broadcast[axis] = True
+        if any(size is None for size in shape):
+            raise ValueError("BatchNormLayer needs specified input sizes for "
+                             "all dimensions/axes not normalized over.")
+        dtype = theano.config.floatX
+        self.mean = self.add_param(lasagne.init.Constant(0), shape, 'mean',
+                                   trainable=False, regularizable=False)
+        self.std = self.add_param(lasagne.init.Constant(1), shape, 'std',
+                                  trainable=False, regularizable=False)
+        self.beta = self.add_param(lasagne.init.Constant(0), shape, 'beta',
+                                   trainable=True, regularizable=True)
+        self.gamma = self.add_param(lasagne.init.Constant(1), shape, 'gamma',
+                                    trainable=True, regularizable=False)
+
+    def get_output_for(self, input, deterministic=False, **kwargs):
+        if deterministic:
+            # use stored mean and std
+            mean = self.mean
+            std = self.std
+        else:
+            # use this batch's mean and std
+            mean = input.mean(self.axes, keepdims=True)
+            std = input.std(self.axes, keepdims=True)
+            # and update the stored mean and std:
+            # we create (memory-aliased) clones of the stored mean and std
+            running_mean = theano.clone(self.mean, share_inputs=False)
+            running_std = theano.clone(self.std, share_inputs=False)
+            # set a default update for them
+            running_mean.default_update = ((1 - self.alpha) * running_mean +
+                                           self.alpha * mean)
+            running_std.default_update = ((1 - self.alpha) * running_std +
+                                          self.alpha * std)
+            # and include them in the graph so their default updates will be
+            # applied (although the expressions will be optimized away later)
+            mean += 0 * running_mean
+            std += 0 * running_std
+        std += self.epsilon
+        mean = T.addbroadcast(mean, *self.axes)
+        std = T.addbroadcast(std, *self.axes)
+        beta = T.addbroadcast(self.beta, *self.axes)
+        gamma = T.addbroadcast(self.gamma, *self.axes)
+        normalized = (input - mean) * (gamma / std) + beta
+        return self.nonlinearity(normalized)
+
+def batch_norm(layer):
+    """
+    Convenience function to apply batch normalization to a given layer's output.
+    Will steal the layer's nonlinearity if there is one (effectively introducing
+    the normalization right before the nonlinearity), and will remove the
+    layer's bias if there is one (because it would be redundant).
+
+    @param layer: The `Layer` instance to apply the normalization to; note that
+        it will be irreversibly modified as specified above
+    @return: A `BatchNormLayer` instance stacked on the given `layer`
+    """
+    nonlinearity = getattr(layer, 'nonlinearity', None)
+    if nonlinearity is not None:
+        layer.nonlinearity = lasagne.nonlinearities.identity
+    if hasattr(layer, 'b'):
+        del layer.params[layer.b]
+        layer.b = None
+    return BatchNormLayer(layer, nonlinearity=nonlinearity)
--- mnist_mlp.py
+++ mnist_mlp.py
@@ -1,70 +1,197 @@
-from __future__ import absolute_import
 from __future__ import print_function
+
+import sys
+import os
+import time
+
 import numpy as np
-np.random.seed(1337)  # for reproducibility
+np.random.seed(1234)  # for reproducibility
 
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
-from keras.layers.normalization import BatchNormalization
-from keras.optimizers import SGD, Adam, RMSprop
-from keras.utils import np_utils
+import theano
+import theano.tensor as T
 
-batch_size = 100
-nb_classes = 10
-nb_epoch = 100
+import lasagne
 
-# the data, shuffled and split between tran and test sets
-(X_train, y_train), (X_valid, y_valid), (X_test, y_test) = mnist.load_data(path="/data/lisa/data/mnist/mnist.pkl")
+import cPickle as pickle
+import gzip
 
-X_train = X_train.reshape(50000, 784)
-X_valid = X_valid.reshape(10000, 784)
-X_test = X_test.reshape(10000, 784)
-X_train = X_train.astype("float32")
-X_valid = X_valid.astype("float32")
-X_test = X_test.astype("float32")
-print(X_train.shape[0], 'train samples')
-print(X_valid.shape[0], 'valid samples')
-print(X_test.shape[0], 'test samples')
+from batch_norm import BatchNormLayer
 
-# TODO preprocessing = center
+if __name__ == "__main__":
+    
+    # BN parameters
+    # alpha = .1 # for a minibatch of size 50
+    # alpha = .2 # for a minibatch of size 100
+    alpha = .33 # for a minibatch of size 200
+    epsilon = 1e-4
+    
+    # MLP parameters
+    num_units = 1024
+    n_hidden_layers = 3
+    
+    # Training parameters
+    num_epochs = 500
+    batch_size = 200
+    
+    print('Loading MNIST dataset...')
 
-# convert class vectors to binary class matrices
-Y_train = np_utils.to_categorical(y_train, nb_classes)
-Y_valid = np_utils.to_categorical(y_valid, nb_classes)
-Y_test = np_utils.to_categorical(y_test, nb_classes)
+    # We'll then load and unpickle the file.
+    filename = "/data/lisa/data/mnist/mnist.pkl.gz"
+    with gzip.open(filename, 'rb') as f:
+        data = pickle.load(f)
 
-# for hinge loss
-Y_train = 2* Y_train - 1.
-Y_valid = 2* Y_valid - 1.
-Y_test = 2* Y_test - 1.
+    # The MNIST dataset we have here consists of six numpy arrays:
+    # Inputs and targets for the training set, validation set and test set.
+    X_train, y_train = data[0]
+    X_val, y_val = data[1]
+    X_test, y_test = data[2]
 
-model = Sequential()
-# model.add(Dropout(0.2))
-model.add(Dense(784, 1024,init='glorot_uniform'))
-model.add(BatchNormalization(input_shape=(1024,),epsilon=1e-4, momentum=0.))
-model.add(Activation('relu'))
-# model.add(Dropout(0.5))
-model.add(Dense(1024, 1024,init='glorot_uniform'))
-model.add(BatchNormalization(input_shape=(1024,),epsilon=1e-4, momentum=0.))
-model.add(Activation('relu'))
-# model.add(Dropout(0.5))
-model.add(Dense(1024, 1024,init='glorot_uniform'))
-model.add(BatchNormalization(input_shape=(1024,),epsilon=1e-4, momentum=0.))
-model.add(Activation('relu'))
-# model.add(Dropout(0.5))
-model.add(Dense(1024, 10))
-model.add(BatchNormalization(input_shape=(10,),epsilon=1e-4, momentum=0.))
-model.add(Activation('linear'))
+    # The inputs come as vectors, we reshape them to monochrome 2D images,
+    # according to the shape convention: (examples, channels, rows, columns)
+    X_train = X_train.reshape((-1, 1, 28, 28))
+    X_val = X_val.reshape((-1, 1, 28, 28))
+    X_test = X_test.reshape((-1, 1, 28, 28))
+    
+    # standardize the dataset
+    def standardize(X):
+        X -= X.mean(axis=0)
+        X /= (X.std(axis=0)+epsilon)
+        return X
+    X_train = standardize(X_train)
+    X_val = standardize(X_val)
+    X_test = standardize(X_test)
 
-# sgd = SGD(lr=0.003)
-# model.compile(loss='squared_hinge', optimizer=sgd)
-adam = Adam()
-model.compile(loss='squared_hinge', optimizer=adam)
-# rms = RMSprop()
-# model.compile(loss='squared_hinge', optimizer=rms)
+    # flatten the targets
+    y_train = np.hstack(y_train)
+    y_val = np.hstack(y_val)
+    y_test = np.hstack(y_test)
 
-model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch, show_accuracy=True, verbose=2, validation_data=(X_valid, Y_valid))
-score = model.evaluate(X_test, Y_test, show_accuracy=True, verbose=0)
-print('Test score:', score[0])
-print('Test accuracy:', score[1])
+    # Onehot the targets
+    y_train = np.float32(np.eye(10)[y_train])    
+    y_val = np.float32(np.eye(10)[y_val])
+    y_test = np.float32(np.eye(10)[y_test])
+
+    # prepare targets for hinge loss
+    y_train = 2* y_train - 1.
+    y_val = 2* y_val - 1.
+    y_test = 2* y_test - 1.
+
+    print('Building the MLP...') 
+    
+    # Prepare Theano variables for inputs and targets
+    input = T.tensor4('inputs')
+    target = T.matrix('targets')
+
+    mlp = lasagne.layers.InputLayer(
+            shape=(None, 1, 28, 28),
+            input_var=input)
+
+    mlp = lasagne.layers.DropoutLayer(
+            mlp, 
+            p=0.2)
+    
+    for k in range(n_hidden_layers):
+    
+        mlp = lasagne.layers.DenseLayer(
+                mlp, 
+                nonlinearity=lasagne.nonlinearities.identity,
+                num_units=num_units)    
+        
+        mlp = BatchNormLayer(
+                mlp,
+                epsilon=epsilon, 
+                alpha=alpha,
+                nonlinearity=lasagne.nonlinearities.rectify)
+                
+        mlp = lasagne.layers.DropoutLayer(
+                mlp, 
+                p=0.5)
+
+    mlp = lasagne.layers.DenseLayer(
+            mlp, 
+            nonlinearity=lasagne.nonlinearities.identity,
+            num_units=10)
+            
+    mlp = BatchNormLayer(
+            mlp,
+            epsilon=epsilon, 
+            alpha=alpha,
+            nonlinearity=lasagne.nonlinearities.identity)
+
+    train_output = lasagne.layers.get_output(mlp)
+    # squared hinge loss
+    loss = T.mean(T.sqr(T.maximum(0.,1.-target*train_output)))
+    params = lasagne.layers.get_all_params(mlp, trainable=True)
+    # updates = lasagne.updates.nesterov_momentum(loss, params, learning_rate=0.1, momentum=0.9)
+    updates = lasagne.updates.adam(loss, params)
+
+    test_output = lasagne.layers.get_output(mlp, deterministic=True)
+    test_loss = T.mean(T.sqr(T.maximum(0.,1.-target*test_output)))
+    test_err = T.mean(T.neq(T.argmax(test_output, axis=1), T.argmax(target, axis=1)),dtype=theano.config.floatX)
+
+    # Compile a function performing a training step on a mini-batch (by giving the updates dictionary) 
+    # and returning the corresponding training loss:
+    train_fn = theano.function([input, target], loss, updates=updates)
+
+    # Compile a second function computing the validation loss and accuracy:
+    val_fn = theano.function([input, target], [test_loss, test_err])
+
+    print('Training...')
+    
+    def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
+        assert len(inputs) == len(targets)
+        if shuffle:
+            indices = np.arange(len(inputs))
+            np.random.shuffle(indices)
+        for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
+            if shuffle:
+                excerpt = indices[start_idx:start_idx + batchsize]
+            else:
+                excerpt = slice(start_idx, start_idx + batchsize)
+            yield inputs[excerpt], targets[excerpt]
+
+    # We iterate over epochs:
+    for epoch in range(num_epochs):
+
+        # In each epoch, we do a full pass over the training data:
+        train_loss = 0
+        train_batches = 0
+        start_time = time.time()
+        for batch in iterate_minibatches(X_train, y_train, batch_size, shuffle=True):
+            inputs, targets = batch
+            train_loss += train_fn(inputs, targets)
+            train_batches += 1
+
+        # And a full pass over the validation data:
+        val_err = 0
+        val_loss = 0
+        val_batches = 0
+        for batch in iterate_minibatches(X_val, y_val, batch_size, shuffle=False):
+            inputs, targets = batch
+            loss, err = val_fn(inputs, targets)
+            val_err += err
+            val_loss += loss
+            val_batches += 1
+
+        # Then we print the results for this epoch:
+        print("Epoch {} of {} took {:.3f}s".format(epoch + 1, num_epochs, time.time() - start_time))
+        print("  training loss:\t\t{:.6f}".format(train_loss / train_batches))
+        print("  validation loss:\t\t{:.6f}".format(val_loss / val_batches))
+        print("  validation error rate:\t{:.2f} %".format(val_err / val_batches * 100))
+
+    # After training, we compute and print the test error:
+    test_err = 0
+    test_loss = 0
+    test_batches = 0
+    for batch in iterate_minibatches(X_test, y_test, batch_size, shuffle=False):
+        inputs, targets = batch
+        loss, err = val_fn(inputs, targets)
+        test_err += err
+        test_loss += loss
+        test_batches += 1
+    print("Final results:")
+    print("  test loss:\t\t\t{:.6f}".format(test_loss / test_batches))
+    print("  test error rate:\t\t{:.2f} %".format(test_err / test_batches * 100))
+
+    # Optionally, you could now dump the network weights to a file like this:
+    # np.savez('model.npz', lasagne.layers.get_all_param_values(network))
